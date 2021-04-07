59476397e918759918868c0a24224f604f84d8f3
Andrew <andrew8gmf@gmail.com>
2021-04-07 11:14:16 -0300

    Add: Convert MongoDB documents to .csv and .xlsx

---
+++ writer.py
@@ -0,0 +1,25 @@
+import os
+import csv
+import pandas
+from openpyxl.workbook import Workbook
+from pymongo import MongoClient
+
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+ATLAS_URI = os.environ.get("ATLAS_URI")
+
+connection = MongoClient(ATLAS_URI)
+db = connection["test"]
+
+cursor = db.data.find()
+mongo_docs = list(cursor)
+
+docs = pandas.DataFrame(columns=[])
+for num, doc in enumerate( mongo_docs ):
+    doc["_id"] = str(doc["_id"])
+    doc_id = doc["_id"]
+    series_obj = pandas.Series( doc, name=doc_id )
+    docs = docs.append( series_obj )
+
+docs.to_csv("input.csv", ",")
+docs.to_excel("output.xlsx")
