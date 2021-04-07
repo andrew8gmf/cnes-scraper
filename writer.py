afb2d9b3de732edef4c93c202d98b541a7eb4f21
Andrew <andrew8gmf@gmail.com>
2021-04-07 11:15:55 -0300

    Update: Docs created in the data folder

--- writer.py
+++ writer.py
@@ -21,5 +21,5 @@ for num, doc in enumerate( mongo_docs ):
     series_obj = pandas.Series( doc, name=doc_id )
     docs = docs.append( series_obj )
 
-docs.to_csv("input.csv", ",")
-docs.to_excel("output.xlsx")
+docs.to_csv("./data/input.csv", ",")
+docs.to_excel("./data/output.xlsx")
